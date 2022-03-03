0350b228812e217c9fe8745805fecca538e57138
Alain Diaz <jdiazcar.3it@bancochile.cl>
2022-03-02 22:40:46 -0300

    ADD styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,10 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: [
+    './app.component.css'
+    
+  ]
 })
 export class AppComponent {
   title = 'spacex-app';
